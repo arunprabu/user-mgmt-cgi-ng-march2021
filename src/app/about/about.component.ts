6d2719346a653f5c2ed346062cc0f35e2688abce
Arun Vijayarengan <arunprabu.v@hotmail.com>
2021-03-05 18:14:44 +0530

    updated

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  today: Date = new Date();
+  // test
+
+  randomText = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. ';
+
   constructor() { }
 
   ngOnInit(): void {
